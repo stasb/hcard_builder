61eac785ed6e182980bafc76850c1974985d5fcb
Stas Belkov <stanislav.belkov@terem.com.au>
2016-10-24 00:33:32 +1100

    Complete markup and styling of the card profile form.

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -7,90 +7,116 @@ class CardForm extends Component {
 
   render() {
     return (
-      <div className="d-hc-form-container">
-        <h1>
-          hCard Builder
-        </h1>
+      <div className="d-hc-form-container-outer">
+        <div className="d-hc-form-container-inner">
+          <h1>
+            hCard Builder
+          </h1>
 
-        <Form className="d-hc-form" model="profile" onSubmit={() => this.handleSubmit()}>
-          <p className="d-hc-form-section-title">
-            personal details
-          </p>
+          <Form className="d-hc-form" model="profile" onSubmit={() => this.handleSubmit()}>
+            <p className="d-hc-form-section-title">
+              personal details
+            </p>
 
-          <div className="d-hc-form-field-row">
-            <Field model="profile.givenName">
-              <label>Given name</label>
-              <input type="text" />
-            </Field>
+            <div className="d-hc-form-field-row">
+              <div className="d-hc-field-col">
+                <Field className="d-hc-field-group" model="profile.givenName">
+                  <label>given name</label>
+                  <input type="text" />
+                </Field>
+              </div>
 
-            <Field model="profile.surname">
-              <label>Surname</label>
-              <input type="text" />
-            </Field>
-          </div>
+              <div className="d-hc-field-col">
+                <Field className="d-hc-field-group" model="profile.surname">
+                  <label>surname</label>
+                  <input type="text" />
+                </Field>
+              </div>
+            </div>
 
-          <div className="d-hc-form-field-row">
-            <Field model="profile.email">
-              <label>Email</label>
-              <input type="text" />
-            </Field>
+            <div className="d-hc-form-field-row">
+              <div className="d-hc-field-col">
+                <Field model="profile.email">
+                  <label>email</label>
+                  <input type="text" />
+                </Field>
+              </div>
 
-            <Field model="profile.phone">
-              <label>Phone</label>
-              <input type="text" />
-            </Field>
-          </div>
+              <div className="d-hc-field-col">
+                <Field model="profile.phone">
+                  <label>phone</label>
+                  <input type="text" />
+                </Field>
+              </div>
+            </div>
 
-          <p className="d-hc-form-section-title">
-            address
-          </p>
+            <p className="d-hc-form-section-title">
+              address
+            </p>
 
-          <div className="d-hc-form-field-row">
-            <Field model="profile.houseName">
-              <label>house name or #</label>
-              <input type="text" />
-            </Field>
+            <div className="d-hc-form-field-row">
+              <div className="d-hc-field-col">
+                <Field model="profile.houseName">
+                  <label>house name or #</label>
+                  <input type="text" />
+                </Field>
+              </div>
 
-            <Field model="profile.street">
-              <label>street</label>
-              <input type="text" />
-            </Field>
-          </div>
+              <div className="d-hc-field-col">
+                <Field model="profile.street">
+                  <label>street</label>
+                  <input type="text" />
+                </Field>
+              </div>
+            </div>
 
-          <div className="d-hc-form-field-row">
-            <Field model="profile.suburb">
-              <label>suburb</label>
-              <input type="text" />
-            </Field>
+            <div className="d-hc-form-field-row">
+              <div className="d-hc-field-col">
+                <Field model="profile.suburb">
+                  <label>suburb</label>
+                  <input type="text" />
+                </Field>
+              </div>
 
-            <Field model="profile.state">
-              <label>state</label>
-              <input type="text" />
-            </Field>
-          </div>
+              <div className="d-hc-field-col">
+                <Field model="profile.state">
+                  <label>state</label>
+                  <input type="text" />
+                </Field>
+              </div>
+            </div>
 
-          <div className="d-hc-form-field-row">
-            <Field model="profile.postcode">
-              <label>postcode</label>
-              <input type="text" />
-            </Field>
+            <div className="d-hc-form-field-row">
+              <div className="d-hc-field-col">
+                <Field model="profile.postcode">
+                  <label>postcode</label>
+                  <input type="text" />
+                </Field>
+              </div>
 
-            <Field model="profile.country">
-              <label>country</label>
-              <input type="text" />
-            </Field>
-          </div>
+              <div className="d-hc-field-col">
+                <Field model="profile.country">
+                  <label>country</label>
+                  <input type="text" />
+                </Field>
+              </div>
+            </div>
 
-          <div className="d-hc-form-action-row">
-            <button>
-              Upload Avatar
-            </button>
+            <div className="d-hc-form-field-row d-hc-action-row">
+              <div className="d-hc-field-col">
+                <button className="d-hc-button d-hc-button-base">
+                  Upload Avatar
+                </button>
+              </div>
 
-            <button type="submit">
-              Create hCard
-            </button>
-          </div>
-        </Form>
+              <div className="d-hc-field-col">
+                <button className="d-hc-button d-hc-button-primary">
+                  Create hCard
+                </button>
+              </div>
+            </div>
+          </Form>
+        </div>
       </div>
     );
   }
